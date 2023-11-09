63ef475c9c3c176755fc2938e503b7064e0c685c
barbz <eduardob907@gmail.com>
2023-11-09 20:43:48 +0100

    remove lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,12 @@ repositories {
 	mavenCentral()
 }
 
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
 	implementation("org.slf4j:slf4j-api:2.0.9")
-
-	compileOnly("org.projectlombok:lombok:1.18.30")
 
 	testImplementation(platform("org.junit:junit-bom:5.9.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
