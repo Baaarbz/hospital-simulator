0b3f0d505339370c632217d62d0a20e514fad30b
barbz <eduardob907@gmail.com>
2023-11-09 20:44:08 +0100

    remove unneeded printer repository

--- src/main/java/com/barbzdev/hospitalsimulator/domain/ResponsePrinter.java
+++
@@ -1,9 +0,0 @@
-package com.barbzdev.hospitalsimulator.domain;
-
-import com.barbzdev.hospitalsimulator.domain.HealthStateEnum;
-
-import java.util.List;
-
-public interface ResponsePrinter {
-	void print(List<HealthStateEnum> patientHealthStates);
-}
--- src/main/java/com/barbzdev/hospitalsimulator/infrastructure/printer/TerminalResponsePrinter.java
+++ src/main/java/com/barbzdev/hospitalsimulator/infrastructure/printer/TerminalResponsePrinter.java
@@ -1,14 +1,13 @@
 package com.barbzdev.hospitalsimulator.infrastructure.printer;
 
 import com.barbzdev.hospitalsimulator.domain.HealthStateEnum;
-import com.barbzdev.hospitalsimulator.domain.ResponsePrinter;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@NoArgsConstructor
-public class TerminalResponsePrinter implements ResponsePrinter {
-	@Override
+public class TerminalResponsePrinter {
+
+	public TerminalResponsePrinter() {
+	}
+
 	public void print(List<HealthStateEnum> patientHealthStates) {
 		// TODO
 	}
