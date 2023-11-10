a19975132679608b49f4ac53251d9fb75b5682a1
barbz <eduardob907@gmail.com>
2023-11-10 16:31:57 +0100

    add error handling InputHandler

--- src/main/java/com/barbzdev/hospitalsimulator/infrastructure/handler/InputHandler.java
+++ src/main/java/com/barbzdev/hospitalsimulator/infrastructure/handler/InputHandler.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -41,14 +42,30 @@ public class InputHandler {
 		terminalResponsePrinter.print(treatedPatients);
 	}
 
-	private final Function<String, List<HealthStateEnum>> transformHealthStateInputFunction = healthStateInput -> Arrays
-		.stream(healthStateInput.toUpperCase().split(","))
-		.map(HealthStateEnum::valueOf)
+	private final Function<String, List<HealthStateEnum>> transformHealthStateInputFunction = healthStatesInput -> Arrays
+		.stream(healthStatesInput.toUpperCase().split(","))
+		.map(healthStateInput -> {
+			try {
+				return HealthStateEnum.valueOf(healthStateInput);
+			} catch (IllegalArgumentException e) {
+				logger.warn("Ignoring patient with health state::{} is not available in the list of health state {}", healthStateInput, HealthStateEnum.values());
+				return null;
+			}
+		})
+		.filter(Objects::nonNull)
 		.toList();
 
 	private final Function<String, Set<DrugEnum>> transformDrugInputFunction = drugsInput -> Arrays
 		.stream(drugsInput.toUpperCase().split(","))
 		.filter(i -> !i.isBlank())
-		.map(DrugEnum::valueOf)
+		.map(drugInput -> {
+			try {
+				return DrugEnum.valueOf(drugInput);
+			} catch (IllegalArgumentException e) {
+				logger.warn("Ignoring drug::{} is not available in the list of drugs {}", drugInput, DrugEnum.values());
+				return null;
+			}
+		})
+		.filter(Objects::nonNull)
 		.collect(Collectors.toSet());
 }
